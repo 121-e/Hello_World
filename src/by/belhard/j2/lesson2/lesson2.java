8c7b4e64de10c3e51f7850a3fce3b22f5425e9f1
121-e <121-e@mail.ru>
2019-04-05 21:34:41 +0300

    1

--- src/by/belhard/j2/lesson2/lesson2.java
+++ src/by/belhard/j2/lesson2/lesson2.java
@@ -1,7 +1,19 @@
 package by.belhard.j2.lesson2;
 
 public class lesson2 {
+
     public static void main(String[] args) {
-int x=1
+        int x=22;
+        if (x==20){
+            x--;
+        } else if (x==21) {
+            x++;
+        } else {
+            x /=2;
+        }
+
+//        System.out.println(x);
+        System.out.println ((x % 2 == 0) ? (x / 2) : (-x));
+
     }
 }
